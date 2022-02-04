f023cfc03f226ba4fd2f69a2f0b8724788faefc7
Brennan Curtis <62488561+Brennan-W-Curtis@users.noreply.github.com>
2022-02-04 18:04:05 -0500

    Create Footer.js
    Created a footer component.

---
+++ Web Development Bootcamp/Brennan-Curtis-Project-Three/src/components/Footer.js
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return (
+        <footer>
+            <div className="wrapper">
+                <p>Created by Brennan Curtis at Copyright &copy; 2021 Juno College</p>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
